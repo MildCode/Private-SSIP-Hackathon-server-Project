ca24e30540c029c800be420b6af2fd53ee0c77e1
bhavya <bhavyajoshi324@gmail.com>
2025-03-07 20:20:44 +0530

    commit

--- department-dashboard/assets/js/All_links.js
+++ department-dashboard/assets/js/All_links.js
@@ -1,12 +1,27 @@
-document.getElementById('login-form').addEventListener('click', function(e) {
+document.getElementById('btn-submit').addEventListener('click', function(e) {
     e.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const department = document.getElementById('department').value;
     const role = document.getElementById('role').value;
-    console.log(username, password, department, role);
+    // console.log(username, password, department, role);
     
-    if (username === 'admin' && password === 'admin' && department === 'admin' && role === 'admin') {
-        window.location.href = 'dashboard.html';
+    if (role === 'hod') {
+        fetch('https://hod-api-2.vercel.app/api/hods')
+            .then(response => response.json())
+            .then(data => {
+                const hod = data.find(hod => hod.name === username);
+                if (hod) {
+                    // if (hod.password === password) {
+                        document.location.assign('/dashboard.html');
+                    // }
+                    // document.location.assign('/hod.html');
+                } else {
+                    alert('HOD not found');
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    } else if (username === 'admin' && password === 'admin' && department === 'admin' && role === 'admin') {
+        document.location.assign('/dashboard.html');
     }
 });
